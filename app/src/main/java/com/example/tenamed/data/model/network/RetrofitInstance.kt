927e1e57854b9fe2f41500bacb726fd416b88076
Bethel Gelan <bethelgelan16@gmail.com>
2025-05-10 10:38:48 +0300

    Create RetrofitInstance.kt

---
+++ app/src/main/java/com/example/tenamed/data/model/network/RetrofitInstance.kt
@@ -0,0 +1,21 @@
+package com.example.tenamed.data.model.network
+
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Retrofit
+import kotlin.jvm.java
+
+object RetrofitInstance {
+    private const val BASE_URL = "http://10.0.2.2:5000"
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
+
+    val api: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
