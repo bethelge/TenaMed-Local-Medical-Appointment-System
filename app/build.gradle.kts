515cbf8635d0ff9f6931aa0d8afc58d50ff27412
Meklit Workineh Daba <133477698+mycodebook123@users.noreply.github.com>
2025-05-10 15:07:02 +0300

    edit some mis cofiguratioins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tena"
+    namespace = "com.example.tenamed"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.tena"
+        applicationId = "com.example.tenamed"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "tena"
+rootProject.name = "tenamed"
 include(":app")
  
