4249bf43811ff37e27cb95e178f8c8d65910b8dc
Bethel Gelan <bethelgelan16@gmail.com>
2025-05-10 10:46:57 +0300

    Create AuthRepository.kt

---
+++ app/src/main/java/com/example/tenamed/data/repository/AuthRepository.kt
@@ -0,0 +1,38 @@
+package com.example.tenamed.data.repository
+
+
+
+import com.example.tenamed.data.model.LoginRequest
+import com.example.tenamed.data.model.LoginResponse
+import com.example.tenamed.data.model.SignupRequest
+import com.example.tenamed.data.model.SignupResponse
+import com.example.tenamed.data.network.RetrofitInstance
+import retrofit2.Response
+
+
+class AuthRepository {
+
+
+    suspend fun registerDoctor(request: SignupRequest): Response<SignupResponse> {
+        return RetrofitInstance.api.registerDoctor(request)
+    }
+
+    // Login doctor
+    suspend fun loginDoctor(request: LoginRequest): Response<LoginResponse> {
+        return RetrofitInstance.api.loginDoctor(request)
+    }
+
+    suspend fun loginPatient(request: LoginRequest): Response<LoginResponse> {
+        return RetrofitInstance.api.loginPatient(request)
+    }
+
+    suspend fun registerPatient(request: SignupRequest): Response<SignupResponse> {
+        return RetrofitInstance.api.registerPatient(request)
+    }
+
+
+
+}
+
+
+
